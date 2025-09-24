d160247b6c6a34be030e18c10cf6d5fb188ab3f2
el2toro <vasea.badragan@gmail.com>
2025-09-24 19:02:17 +0200

    Add task repository and implementation

---
+++ Services/Project.API/Repository/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using Project.API.Models;
+
+namespace Project.API.Repository;
+
+public interface ITaskRepository
+{
+    Task<TaskItem> CreateTaskAsync(TaskItem task, CancellationToken cancellationToken);
+    Task<IEnumerable<TaskItem>> GeTasksAsync(Guid projectId, CancellationToken cancellationToken);
+    Task<TaskItem> GetTaskByIdAsync(Guid taskId, CancellationToken cancellationToken);
+    Task<TaskItem> UpdateTaskAsync(TaskItem taskDto, CancellationToken cancellationToken);
+}
---
+++ Services/Project.API/Repository/TaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using Project.API.Data;
+using Project.API.Dtos;
+using Project.API.Models;
+
+namespace Project.API.Repository;
+
+public class TaskRepository(ProjectDbContext dbContext) : ITaskRepository
+{
+    private readonly ProjectDbContext _dbContext = dbContext;
+
+    public async Task<TaskItem> CreateTaskAsync(TaskItem task, CancellationToken cancellationToken)
+    {
+        var createdTask = _dbContext.Tasks.Add(task).Entity;
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return createdTask;
+    }
+
+    public Task<IEnumerable<TaskItem>> GeTasksAsync(Guid projectId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TaskItem> GetTaskByIdAsync(Guid taskId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TaskItem> UpdateTaskAsync(TaskItem task, CancellationToken cancellationToken)
+    {
+        var existingTask = _dbContext.Tasks.Find(task.Id);
+
+        ArgumentNullException.ThrowIfNull(existingTask, nameof(existingTask));
+
+        existingTask.Title = task.Title;
+        existingTask.Description = task.Description;
+        existingTask.DueDate = task.DueDate;
+        existingTask.TaskStatus = task.TaskStatus;
+        existingTask.AssignedTo = task.AssignedTo;
+        existingTask.UpdatedAt = DateTime.UtcNow;
+        existingTask.UpdatedBy = task.UpdatedBy;
+
+        //TODO: Add subtasks and comments mapping
+
+        _dbContext.Tasks.Update(existingTask);
+        await _dbContext.SaveChangesAsync();
+
+        return existingTask;
+    }
+}
