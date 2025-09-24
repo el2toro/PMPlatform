f554d540b76c7cf6f51f0386ac2e2786d15abd6b
el2toro <vasea.badragan@gmail.com>
2025-09-24 19:02:42 +0200

    Implement create task feature

--- Services/Project.API/Project/Task/CreateTask/CreateTaskEndpoint.cs
+++ Services/Project.API/Project/Task/CreateTask/CreateTaskEndpoint.cs
@@ -1,6 +1,22 @@
-﻿namespace Project.API.Project.Task.CreateTask
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Project.API.Dtos;
+
+namespace Project.API.Project.Task.CreateTask;
+
+public record CreateTaskRequest(Guid ProjectId, string Title, string Description, DateTime DueDate);
+
+public class CreateTaskEndpoint : ICarterModule
 {
-    public class CreateTaskEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("api/tasks", async ([FromBody] TaskItemDto request, ISender sender) =>
+        {
+            // var command = request.Adapt<CreateTaskCommand>();
+            var response = await sender.Send(new CreateTaskCommand(request));
+            return Results.Ok(response.Task);
+        });
     }
 }
--- Services/Project.API/Project/Task/CreateTask/CreateTaskHandler.cs
+++ Services/Project.API/Project/Task/CreateTask/CreateTaskHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace Project.API.Project.Task.CreateTask
+﻿using Mapster;
+using MediatR;
+using Project.API.Dtos;
+using Project.API.Models;
+using Project.API.Repository;
+
+namespace Project.API.Project.Task.CreateTask;
+
+public record CreateTaskCommand(TaskItemDto Task) : IRequest<CreateTaskResult>;
+public record CreateTaskResult(TaskItemDto Task);
+
+public class CreateTaskHandler(ITaskRepository taskRepository)
+    : IRequestHandler<CreateTaskCommand, CreateTaskResult>
 {
-    public class CreateTaskHandler
+    public async Task<CreateTaskResult> Handle(CreateTaskCommand command, CancellationToken cancellationToken)
     {
+        var taskToCreate = command.Task.Adapt<TaskItem>();
+        taskToCreate.CreatedAt = DateTime.UtcNow;
+        //TODO: Replace with actual logged in user
+        taskToCreate.CreatedBy = Guid.Parse("2ED46488-5667-42E6-A3B1-0046AB4BFCA6");
+        taskToCreate.UpdatedBy = Guid.Parse("2ED46488-5667-42E6-A3B1-0046AB4BFCA6");
+        var createdTask = await taskRepository.CreateTaskAsync(taskToCreate, cancellationToken);
+        var result = createdTask.Adapt<TaskItemDto>();
+
+        return new CreateTaskResult(result);
     }
 }
