0d3295293c668091412b099229be8d7b29bbe986
el2toro <vasea.badragan@gmail.com>
2025-09-26 18:15:43 +0200

    Add services to the tenant service client

--- Services/AuthService/Services/TenantServiceClient.cs
+++ Services/AuthService/Services/TenantServiceClient.cs
@@ -1,13 +1,8 @@
 ﻿namespace Auth.API.Services;
 
-public class TenantServiceClient
+public class TenantServiceClient(HttpClient httpClient)
 {
-    private readonly HttpClient _httpClient;
-
-    public TenantServiceClient(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
+    private readonly HttpClient _httpClient = httpClient;
 
     public async Task<Tenant> CreateTenant(Tenant tenant)
     {
@@ -27,7 +22,35 @@ public class TenantServiceClient
 
         response.EnsureSuccessStatusCode();
 
-        // Optionally, handle the response content if needed
         return await response.Content.ReadFromJsonAsync<Tenant>() ?? new Tenant();
+    }
+
+    public async Task<Tenant> GetTenantById(Guid tenantId)
+    {
+        var response = await _httpClient.GetAsync($"api/tenants/{tenantId}");
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<Tenant>() ?? new Tenant();
+    }
+
+    public async Task<Tenant?> GetTenantByName(string tenantName)
+    {
+        var response = await _httpClient.GetAsync($"api/tenants/{tenantName}");
+        response.EnsureSuccessStatusCode();
+
+        if (response.Content == null || response.Content.Headers.ContentLength == 0)
+        {
+            return null;
+        }
+
+        return await response.Content.ReadFromJsonAsync<Tenant>(); ;
+    }
+
+    public async Task<IEnumerable<Tenant>> GetAllTenants()
+    {
+        var response = await _httpClient.GetAsync("api/tenants");
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<IEnumerable<Tenant>>() ?? Enumerable.Empty<Tenant>();
     }
 }
