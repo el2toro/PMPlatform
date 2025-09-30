3990c7d9115703e5d7b4d6b13056a8757cab9e1d
el2toro <vasea.badragan@gmail.com>
2025-09-30 09:50:42 +0200

    Implement GetTaskByComunId endpoint and handler

---
+++ Services/Project.API/Project/Task/GetTasksByColumId/GetTasksByColumnIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using Mapster;
+using MediatR;
+using Project.API.Dtos;
+using Project.API.Repository;
+
+namespace Project.API.Project.Task.GetTasksByColumId;
+
+public record GetTasksByColumnIdQuery(Guid ColumnId) : IRequest<GetTasksByColumnIdResult>;
+
+public record GetTasksByColumnIdResult(IEnumerable<TaskItemDto> Tasks);
+
+public class GetTasksByColumnIdHandler(ITaskRepository taskRepository)
+    : IRequestHandler<GetTasksByColumnIdQuery, GetTasksByColumnIdResult>
+{
+    public async Task<GetTasksByColumnIdResult> Handle(GetTasksByColumnIdQuery query, CancellationToken cancellationToken)
+    {
+        var tasks = await taskRepository.GeTasksByColumnIdAsync(query.ColumnId, cancellationToken);
+        var result = tasks.Adapt<IEnumerable<TaskItemDto>>();
+        return new GetTasksByColumnIdResult(result);
+    }
+}
