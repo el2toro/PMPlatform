6fc981a4f3b9396ab5350135ae583b82c6867736
el2toro <vasea.badragan@gmail.com>
2025-09-30 08:29:58 +0200

    Implement board endpoint and handler

---
+++ Services/Project.API/Project/Board/GetBoard/GetBoardEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+using MediatR;
+
+namespace Project.API.Project.Board.GetBoard;
+
+public class GetBoardEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/projects/{projectId:guid}/board", async (Guid projectId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBoardQuery(projectId));
+
+            return Results.Ok(result.Board);
+        });
+    }
+}
---
+++ Services/Project.API/Project/Board/GetBoard/GetBoardHandler.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MediatR;
+using Project.API.Dtos.Board;
+using Project.API.Repository;
+
+namespace Project.API.Project.Board.GetBoard;
+
+public record GetBoardQuery(Guid ProjectId) : IRequest<GetBoardResult>;
+
+public record GetBoardResult(BoardDto Board);
+
+public class GetBoardHandler(IBoardRepository boardRepository)
+    : IRequestHandler<GetBoardQuery, GetBoardResult>
+{
+    public async Task<GetBoardResult> Handle(GetBoardQuery query, CancellationToken cancellationToken)
+    {
+        var board = await boardRepository.GetBoardAsync(query.ProjectId);
+
+        var dto = new BoardDto()
+        {
+            Id = board.Id,
+            Name = board.Name,
+            Description = board.Description,
+            CreatedAt = board.CreatedAt,
+            CreatedBy = board.CreatedBy,
+            ProjectId = board.ProjectId,
+            Columns = board.Columns.Adapt<IEnumerable<ColumnDto>>()
+        };
+
+        return new GetBoardResult(dto);
+    }
+}
