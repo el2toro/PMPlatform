7f42e73f15af9460fbe17684c35a9a2b205d9f53
el2toro <vasea.badragan@gmail.com>
2025-09-30 08:29:17 +0200

    Add additionals dtos

--- Services/Project.API/Dtos/Board/BoardDto.cs
+++ Services/Project.API/Dtos/Board/BoardDto.cs
@@ -2,4 +2,12 @@
 
 public class BoardDto
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Guid ProjectId { get; set; }
+    public IEnumerable<ColumnDto> Columns { get; set; } = [];
 }
--- Services/Project.API/Dtos/Board/ColumnDto.cs
+++ Services/Project.API/Dtos/Board/ColumnDto.cs
@@ -1,5 +1,11 @@
-﻿namespace Project.API.Dtos.Board;
+﻿using Project.API.Models;
+
+namespace Project.API.Dtos.Board;
 
 public class ColumnDto
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public Guid BoardId { get; set; }
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
 }
