d5c933a6bf76decc24581ad1a3be2d3c6c004b24
el2toro <vasea.badragan@gmail.com>
2025-09-30 08:30:23 +0200

    Implement board repository

---
+++ Services/Project.API/Repository/BoardRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.API.Data;
+using Project.API.Models;
+
+namespace Project.API.Repository;
+
+public class BoardRepository(ProjectDbContext dbContext) : IBoardRepository
+{
+    private readonly ProjectDbContext _dbContext = dbContext;
+
+    public async Task<Board> GetBoardAsync(Guid projectId)
+    {
+        var board = await _dbContext.Boards
+            .Include(b => b.Columns)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(b => b.ProjectId == projectId);
+
+        return board ?? new Board();
+    }
+}
---
+++ Services/Project.API/Repository/IBoardRepository.cs
@@ -0,0 +1,8 @@
+﻿using Project.API.Models;
+
+namespace Project.API.Repository;
+
+public interface IBoardRepository
+{
+    Task<Board> GetBoardAsync(Guid projectId);
+}
