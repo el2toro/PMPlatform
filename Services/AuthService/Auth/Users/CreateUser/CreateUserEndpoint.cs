04a7c33490f57aee0b89f295fa47ab69c26ab4a4
el2toro <vasea.badragan@gmail.com>
2025-10-01 14:16:41 +0200

    Implement CreateUser endpoint and handler

---
+++ Services/AuthService/Auth/Users/CreateUser/CreateUserEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace Auth.API.Auth.Users.CreateUser;
+
+public class CreateUserEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/tenants/{tenantId:guid}/users", async (UserDto request, ISender sender) =>
+        {
+            var result = await sender.Send(new CreateUserCommand(request));
+            return Results.Created($"api/tenants/{"tenantId"}/users/{result.User.Id}", result.User);
+        });
+    }
+}
