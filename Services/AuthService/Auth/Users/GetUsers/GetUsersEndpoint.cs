f9dc2108b26e3152bda5d02c6eebe18a2644cb53
el2toro <vasea.badragan@gmail.com>
2025-10-01 13:27:42 +0200

    Implement GetUsers  handler and endpoint

---
+++ Services/AuthService/Auth/Users/GetUsers/GetUsersEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace Auth.API.Auth.Users.GetUsers;
+
+public class GetUsersEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/users", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetUsersQuery());
+            return Results.Ok(result.Users);
+        });
+    }
+}
---
+++ Services/AuthService/Auth/Users/GetUsers/GetUsersHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using Auth.API.Repository;
+
+namespace Auth.API.Auth.Users.GetUsers;
+
+public record GetUsersQuery() : IRequest<GetUsersResult>;
+public record GetUsersResult(IEnumerable<UserDto> Users);
+
+public class GetUsersHandler(IUserRepository userRepository)
+    : IRequestHandler<GetUsersQuery, GetUsersResult>
+{
+    public async Task<GetUsersResult> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await userRepository.GetUsersAsync(cancellationToken);
+        var result = users.Adapt<IEnumerable<UserDto>>();
+        return new GetUsersResult(result);
+    }
+}
