34af718f9719d4defc9d7c38e51f5c028877e7ba
el2toro <vasea.badragan@gmail.com>
2025-10-02 16:19:42 +0200

    Add enums

---
+++ Services/TaskService/TaskService.Domain/Enums/TaskItemStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskService.Domain.Enums;
+
+public enum TaskItemStatus
+{
+    Backlog = 1,
+    ToDo = 2,
+    InProgress = 3,
+    Done = 4,
+    Blocked = 5,
+    Cancelled = 6,
+    OnHold = 7,
+    Review = 8,
+    Testing = 9
+}
