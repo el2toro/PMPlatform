fa10117be2d9e61f84ecce7cbee864dcb4f3fe24
el2toro <vasea.badragan@gmail.com>
2025-10-02 16:19:56 +0200

    Add interfaces

---
+++ Services/TaskService/TaskService.Domain/Interfaces/ITaskServiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskService.Domain.Entities;
+using TaskService.Domain.Enums;
+
+namespace TaskService.Domain.Interfaces;
+
+public interface ITaskServiceRepository
+{
+    Task<TaskItem> CreateTaskAsync(TaskItem task, CancellationToken cancellationToken);
+    Task<IEnumerable<TaskItem>> GeTasksAsync(Guid projectId, CancellationToken cancellationToken);
+    Task<TaskItem> GetTaskByIdAsync(Guid taskId, CancellationToken cancellationToken);
+    Task<TaskItem> UpdateTaskAsync(TaskItem taskDto, CancellationToken cancellationToken);
+    Task<TaskItem> UpdateTaskStatusAsync(Guid taskId, Guid columnId, TaskItemStatus status, CancellationToken cancellationToken);
+}
