e467c71b6b7140b4ad170e62d012631bc37cbe30
el2toro <vasea.badragan@gmail.com>
2025-10-02 16:19:02 +0200

    Create and implement CreateTask handler

---
+++ Services/TaskService/TaskService.Application/Tasks/Commands/CreateTask/CreateTaskHandler.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using MediatR;
+using TaskService.Application.Dtos;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Interfaces;
+
+namespace TaskService.Application.Tasks.Commands.CreateTask;
+
+public record CreateTaskCommand(TaskItemDto TaskItem) : IRequest<CreateTaskResult>;
+public record CreateTaskResult(TaskItemDto TaskItem);
+
+public class CreateTaskHandler(ITaskServiceRepository taskServiceRepository)
+    : IRequestHandler<CreateTaskCommand, CreateTaskResult>
+{
+    public async Task<CreateTaskResult> Handle(CreateTaskCommand command, CancellationToken cancellationToken)
+    {
+        var taskItem = command.TaskItem.Adapt<TaskItem>();
+
+        foreach (var comment in taskItem.Comments)
+        {
+            comment.CreatedAt = DateTime.UtcNow;
+            comment.UpdatedAt = DateTime.UtcNow;
+        }
+
+        var createdTask = await taskServiceRepository.CreateTaskAsync(taskItem, cancellationToken);
+        var result = createdTask.Adapt<TaskItemDto>();
+
+        return new CreateTaskResult(result);
+    }
+}
