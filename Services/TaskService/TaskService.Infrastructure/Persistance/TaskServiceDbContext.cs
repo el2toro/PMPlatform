c42d8e7d00ca91289c7365d41d66ecd0074a780f
el2toro <vasea.badragan@gmail.com>
2025-10-03 06:41:43 +0200

    Add indexes in db tables

--- Services/TaskService/TaskService.Infrastructure/Persistance/TaskServiceDbContext.cs
+++ Services/TaskService/TaskService.Infrastructure/Persistance/TaskServiceDbContext.cs
@@ -1,7 +1,4 @@
-﻿using TaskService.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace TaskService.Infrastructure.Persistance;
+﻿namespace TaskService.Infrastructure.Persistance;
 
 public class TaskServiceDbContext : DbContext
 {
@@ -19,39 +16,49 @@ public class TaskServiceDbContext : DbContext
         modelBuilder.Entity<TaskItem>(entity =>
         {
             entity.HasKey(t => t.Id);
+            entity.HasIndex(t => t.Id).IsUnique();
             entity.Property(t => t.Title).HasMaxLength(100);
             entity.Property(t => t.Description).HasMaxLength(1000);
 
-            entity.HasMany(t => t.Subtasks)
-            .WithOne(st => st.Task)
-            .HasForeignKey(st => st.TaskId);
+            //entity.HasMany(t => t.Subtasks)
+            //.WithOne(st => st.Task)
+            //.HasForeignKey(st => st.TaskId);
 
-            entity.HasMany(t => t.Comments)
-            .WithOne(c => c.Task)
-            .HasForeignKey(c => c.TaskId);
+            //entity.HasMany(t => t.Comments)
+            //.WithOne(c => c.Task)
+            //.HasForeignKey(c => c.TaskId);
 
-            entity.HasMany(t => t.Attachments)
-            .WithOne(a => a.Task)
-            .HasForeignKey(a => a.TaskId);
+            //entity.HasMany(t => t.Attachments)
+            //.WithOne(a => a.Task)
+            //.HasForeignKey(a => a.TaskId);
         });
 
         modelBuilder.Entity<Subtask>(entity =>
         {
             entity.HasKey(st => st.Id);
+            entity.HasIndex(t => t.Id).IsUnique();
             entity.Property(st => st.Title).HasMaxLength(100);
+
+            entity.HasOne(sb => sb.Task).WithMany(t => t.Subtasks).HasForeignKey(sb => sb.TaskId);
         });
 
         modelBuilder.Entity<Comment>(entity =>
         {
             entity.HasKey(c => c.Id);
+            entity.HasIndex(t => t.Id).IsUnique();
             entity.Property(c => c.Content).HasMaxLength(1000);
+
+            entity.HasOne(c => c.Task).WithMany(t => t.Comments).HasForeignKey(c => c.TaskId);
         });
 
         modelBuilder.Entity<Attachment>(entity =>
         {
             entity.HasKey(a => a.Id);
+            entity.HasIndex(t => t.Id).IsUnique();
             entity.Property(a => a.FileName).HasMaxLength(200);
             entity.Property(a => a.ContentType).HasMaxLength(100);
+
+            entity.HasOne(a => a.Task).WithMany(t => t.Attachments).HasForeignKey(a => a.TaskId);
         });
     }
 }
