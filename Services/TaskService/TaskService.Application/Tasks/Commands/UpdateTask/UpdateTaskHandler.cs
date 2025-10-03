47a75c3c32641cf14bba5aeedad89da9aaf1e133
el2toro <vasea.badragan@gmail.com>
2025-10-03 06:35:34 +0200

    Implement update task handler

---
+++ Services/TaskService/TaskService.Application/Tasks/Commands/UpdateTask/UpdateTaskHandler.cs
@@ -0,0 +1,23 @@
+﻿using Mapster;
+using MediatR;
+using TaskService.Application.Dtos;
+using TaskService.Domain.Entities;
+using TaskService.Domain.Interfaces;
+
+namespace TaskService.Application.Tasks.Commands.UpdateTask;
+
+public record UpdateTaskCommand(TaskItemDto Task) : IRequest<UpdateTaskResult>;
+public record UpdateTaskResult(TaskItemDto Task);
+
+public class UpdateTaskHandler(ITaskServiceRepository taskServiceRepository)
+    : IRequestHandler<UpdateTaskCommand, UpdateTaskResult>
+{
+    public async Task<UpdateTaskResult> Handle(UpdateTaskCommand command, CancellationToken cancellationToken)
+    {
+        var taskItem = command.Task.Adapt<TaskItem>();
+        var updatedTask = await taskServiceRepository.UpdateTaskAsync(taskItem, cancellationToken);
+        var result = updatedTask.Adapt<TaskItemDto>();
+
+        return new UpdateTaskResult(result);
+    }
+}
