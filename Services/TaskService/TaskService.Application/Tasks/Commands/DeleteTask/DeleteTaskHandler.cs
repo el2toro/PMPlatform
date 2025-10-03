3a2cb65c1daae06258756ceec8844fe85d683511
el2toro <vasea.badragan@gmail.com>
2025-10-03 06:34:18 +0200

    Implement delete task handler

---
+++ Services/TaskService/TaskService.Application/Tasks/Commands/DeleteTask/DeleteTaskHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskService.Application.Tasks.Commands.DeleteTask;
+
+public record DeleteTaskCommand(Guid TaskId) : IRequest<DeleteTaskResult>;
+public record DeleteTaskResult(bool IsSuccess);
+
+public class DeleteTaskHandler(ITaskServiceRepository taskServiceRepository)
+    : IRequestHandler<DeleteTaskCommand, DeleteTaskResult>
+{
+    public async Task<DeleteTaskResult> Handle(DeleteTaskCommand command, CancellationToken cancellationToken)
+    {
+        await taskServiceRepository.DeleteTaskAsync(command.TaskId, cancellationToken);
+        return new DeleteTaskResult(true);
+    }
+}
