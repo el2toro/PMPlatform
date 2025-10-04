f20f44fc180d8b8d79349e0422e6f390e0735122
el2toro <vasea.badragan@gmail.com>
2025-10-04 17:16:50 +0200

    Change location for auth service, and remove gRPC services

--- Services/Board.gRPC/Entities/Column.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Board.gRPC.Entities;
-
-public class Column
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = default!;
-    public Guid BoardId { get; set; }
-    public Board Board { get; set; } = default!;
-}
