e27c4172b1818f5e5ce75db223cbb944796a3b5e
el2toro <vasea.badragan@gmail.com>
2025-10-05 09:20:49 +0200

    Add dtos

--- Services/Board/Board.Application/Dtos/BoardDto.cs
+++ Services/Board/Board.Application/Dtos/BoardDto.cs
@@ -2,4 +2,14 @@
 
 public class BoardDto
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string? Description { get; set; } = default!;
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Guid ProjectId { get; set; }
+    public string ProjectName { get; set; } = default!;
+    public string? ProjectDescription { get; set; } = default!;
+    public IEnumerable<ColumnDto> Columns { get; set; } = [];
 }
---
+++ Services/Board/Board.Application/Dtos/ColumnDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Board.Application.Dtos;
+
+public record ColumnDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public Guid BoardId { get; set; }
+}
