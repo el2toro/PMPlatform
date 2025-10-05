927b6b2cc39e9378b7df8d09f746d276051aa2d1
el2toro <vasea.badragan@gmail.com>
2025-10-05 09:19:52 +0200

    Configure mediatr

--- Services/Board/Board.API/Program.cs
+++ Services/Board/Board.API/Program.cs
@@ -1,13 +1,24 @@
+using Board.Application.BoardHandlers.Queries.GetBoardById;
+using Board.Application.Interfaces;
+using Board.Infrastructure.Persistance;
+using Board.Infrastructure.Repositories;
 using Carter;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IBoardRepository, BoardRepository>();
 builder.Services.AddCarter();
 
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(typeof(GetBoardByIdHandler).Assembly);
+});
+
+builder.Services.AddDbContext<BoardDbContext>(config =>
+{
+    config.UseSqlServer(builder.Configuration.GetConnectionString("BoardDb"));
 });
 
 var app = builder.Build();
