5c736a8f987e34a1868521342952c74b95694c23
el2toro <vasea.badragan@gmail.com>
2025-10-05 09:20:04 +0200

    Implement endpoints

--- Services/Board/Board.API/Endpoints/BoardEndpoints.cs
+++ Services/Board/Board.API/Endpoints/BoardEndpoints.cs
@@ -1,4 +1,10 @@
-﻿using Carter;
+﻿using Board.Application.BoardHandlers.Commands.CreateBoard;
+using Board.Application.BoardHandlers.Commands.DeleteBoard;
+using Board.Application.BoardHandlers.Commands.UpdateBoard;
+using Board.Application.BoardHandlers.Queries.GetBoardById;
+using Board.Application.BoardHandlers.Queries.GetBoards;
+using Board.Application.Dtos;
+using Carter;
 using MediatR;
 
 namespace Board.API.Endpoints;
@@ -9,32 +15,32 @@ public class BoardEndpoints : ICarterModule
     {
         app.MapGet("project/{projectId}/boards/{boardId}", async (Guid projectId, Guid boardId, ISender sender) =>
         {
-            var board = await sender.Send("request");
-            return Task.FromResult("it works");
+            var result = await sender.Send(new GetBoardByIdQuery(projectId, boardId));
+            return Results.Ok(result.Board);
         });
 
         app.MapGet("project/{projectId}/boards", async (Guid projectId, ISender sender) =>
         {
-            var board = await sender.Send("request");
-            return Task.FromResult("it works");
+            var result = await sender.Send(new GetBoardsQuery(projectId));
+            return Results.Ok(result.Boards);
         });
 
-        app.MapPost("project/{projectId}/boards", async (Guid projectId, ISender sender) =>
+        app.MapPost("project/{projectId}/boards", async (BoardDto request, ISender sender) =>
         {
-            var board = await sender.Send("request");
-            return Task.FromResult("it works");
+            var result = await sender.Send(new CreateBoardCommand(request));
+            return Results.Created($"project/{result.Board.ProjectId}/boards/{result.Board.Id}", result.Board);
         });
 
-        app.MapPut("project/{projectId}/boards", async (Guid projectId, ISender sender) =>
+        app.MapPut("project/{projectId}/boards", async (BoardDto request, ISender sender) =>
         {
-            var board = await sender.Send("request");
-            return Task.FromResult("it works");
+            var result = await sender.Send(new UpdateBoardCommand(request));
+            return Results.Ok(result.Board);
         });
 
         app.MapDelete("project/{projectId}/boards/{boardId}", async (Guid projectId, Guid boardId, ISender sender) =>
         {
-            var board = await sender.Send("request");
-            return Task.FromResult("it works");
+            var result = await sender.Send(new DeleteBoardCommand(projectId, boardId));
+            return Results.NoContent();
         });
     }
 }
