4969b54a080a609a83088b8014f15f378533e483
el2toro <vasea.badragan@gmail.com>
2025-10-05 09:21:27 +0200

    Define contracts

---
+++ Services/Board/Board.Application/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Board.Application.Dtos;
+global using Board.Application.Interfaces;
+global using Mapster;
+global using MediatR;
--- Services/Board/Board.Application/Interfaces/IBoardRepository.cs
+++ Services/Board/Board.Application/Interfaces/IBoardRepository.cs
@@ -2,5 +2,9 @@
 
 public interface IBoardRepository
 {
-
+    Task<IEnumerable<Domain.Entities.Board>> GetBoardsAsync(Guid projectId, CancellationToken cancellationToken);
+    Task<Domain.Entities.Board> GetBoardByIdAsync(Guid projectId, Guid boardId, CancellationToken cancellationToken);
+    Task<Domain.Entities.Board> CreateBoardAsync(Domain.Entities.Board board, CancellationToken cancellationToken);
+    Task<Domain.Entities.Board> UpdateBoardAsync(Domain.Entities.Board board, CancellationToken cancellationToken);
+    Task DeleteBoardAsync(Guid projectId, Guid boardId, CancellationToken cancellationToken);
 }
