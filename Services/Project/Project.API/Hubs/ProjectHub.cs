6e723a00c28fd6f4bcc28a11c1cc62ab795d8666
el2toro <vasea.badragan@gmail.com>
2025-10-06 12:43:54 +0200

    Implement project hub send project methods

--- Services/Project/Project.API/Hubs/ProjectHub.cs
+++ Services/Project/Project.API/Hubs/ProjectHub.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Project.API.Dtos;
-using Project.API.Models;
-
 namespace Project.API.Hubs;
 
 public interface IProjectHub
 {
-    Task SendMessage(ProjectDto project);
+    Task SendProject(ProjectDto project);
+    Task SendUpdatedProject(ProjectDto project);
 }
 
 public class ProjectHub(IHubContext<ProjectHub> hubContext) : Hub, IProjectHub
 {
     private readonly IHubContext<ProjectHub> _hubContext = hubContext;
-    public async Task SendMessage(ProjectDto project)
+    public async Task SendProject(ProjectDto project)
     {
-        await _hubContext.Clients.All.SendAsync("ReceiveMessage", project);
+        await _hubContext.Clients.All.SendAsync("ReceiveProjects", project);
+    }
+
+    public async Task SendUpdatedProject(ProjectDto project)
+    {
+        await _hubContext.Clients.All.SendAsync("ReceiveUpdatedProject", project);
     }
 }
