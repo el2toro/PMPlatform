9d1f1148df0b0162db671257006c97f267a690a9
el2toro <vasea.badragan@gmail.com>
2025-10-06 12:46:13 +0200

    Delete unused dtos

--- Services/Project/Project.API/Dtos/Board/ColumnDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project.API.Dtos.Board;
-
-public class ColumnDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = default!;
-    public Guid BoardId { get; set; }
-    public IEnumerable<TaskItemDto> Tasks { get; set; } = new List<TaskItemDto>();
-}
--- Services/Project/Project.API/Dtos/SubtaskDto.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Project.API.Dtos;
-
-public record SubtaskDto(Guid Id, Guid TaskId, string Title, bool IsCompleted);
