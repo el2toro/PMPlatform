0844d48d5efbdd9846d58688ff20a154d2261112
el2toro <vasea.badragan@gmail.com>
2025-10-06 12:53:48 +0200

    Remove unused repositories

--- Services/Project/Project.API/Repository/IBoardRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Project.API.Dtos.Board;
-
-namespace Project.API.Repository;
-
-public interface IBoardRepository
-{
-    Task<BoardDto> GetBoardAsync(Guid projectId, CancellationToken cancellationToken);
-}
