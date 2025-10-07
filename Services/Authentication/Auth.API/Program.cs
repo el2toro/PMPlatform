dc81f8ed12459c2d42bfa7bf51f5b461c6491fc7
el2toro <vasea.badragan@gmail.com>
2025-10-07 11:21:56 +0200

    Remove unnecessary implementation

--- Services/Authentication/Auth.API/Program.cs
+++ Services/Authentication/Auth.API/Program.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Auth.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,48 +30,12 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddCarter();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("PMPolicy",
-        policy => policy
-            .WithOrigins("http://localhost:4200") // Angular app URL
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-});
-
-// Configure Authentication
-var jwtKey = builder.Configuration["Jwt:Key"];
-var jwtIssuer = builder.Configuration["Jwt:Issuer"];
-var jwtAudience = builder.Configuration["Jwt:Audience"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtIssuer,
-            ValidAudience = jwtAudience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
-        };
-    });
-
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
-app.UseCors("PMPolicy");
-
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.Run();
 
--- Services/Project/Project.API/Program.cs
+++ Services/Project/Project.API/Program.cs
@@ -1,13 +1,15 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Core.Messaging.MassTransit;
+using Project.API.TenantContext;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IProjectHub, ProjectHub>();
+builder.Services.AddScoped<ITenantContext, TenantContext>();
 
 builder.Services.AddDbContext<ProjectDbContext>(options =>
 {
@@ -25,52 +27,15 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
 
 builder.Services.AddCarter();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("PMPolicy",
-        policy => policy
-            .WithOrigins("http://localhost:4200") // Angular app URL
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-});
-
-// Configure Authentication
-var jwtKey = builder.Configuration["Jwt:Key"];
-var jwtIssuer = builder.Configuration["Jwt:Issuer"];
-var jwtAudience = builder.Configuration["Jwt:Audience"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtIssuer,
-            ValidAudience = jwtAudience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
-        };
-    });
-
 builder.Services.AddMessageBroker(builder.Configuration, Assembly.GetExecutingAssembly());
 
-builder.Services.AddAuthorization();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
-app.UseCors("PMPolicy");
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); //do u need that?
 app.MapCarter();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapHub<ProjectHub>("/hub/project");
 
