93ff0a552a3a053a4207732e48405e3fce9d63b8
el2toro <vasea.badragan@gmail.com>
2025-10-07 11:20:45 +0200

    Add base event

---
+++ BuildingBlocks/Core.Messaging/Events/EventBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Messaging.Events;
+
+public class EventBase<T> where T : class
+{
+    public Guid EventId { get; set; } = Guid.NewGuid();
+    public DateTime OccurredAt { get; set; } = DateTime.Now;
+    public string EventType { get; set; } = default!;
+    public string Origin { get; set; } = default!;
+    public Guid TenantId { get; set; } = default!;
+    public T EventData { get; set; } = default!;
+}
