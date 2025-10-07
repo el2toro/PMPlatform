0ead670103c4c5c07b21ef5902f8e44eaa39fff6
el2toro <vasea.badragan@gmail.com>
2025-10-07 07:07:14 +0200

    Define task service contracts

---
+++ Services/TaskService/TaskService.Application/Interfaces/ITaskServiceHub.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskService.Application.Interfaces;
+
+public interface ITaskServiceHub
+{
+    Task SendCreatedTask(TaskItemDto task);
+    Task SendUpdatedTask(TaskItemDto task);
+    Task SendTaskAssigneChanged(Guid taskId, Guid assignedTo);
+    Task SendTaskStatusChanged(Guid taskId, int status);
+}
