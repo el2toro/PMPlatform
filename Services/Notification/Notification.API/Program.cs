03d788f8d5db128772baf90439fc9ce0fcd5d84a
el2toro <vasea.badragan@gmail.com>
2025-10-11 17:32:03 +0200

    Configure carter

--- Services/Notification/Notification.API/Program.cs
+++ Services/Notification/Notification.API/Program.cs
@@ -1,11 +1,13 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
+app.MapCarter();
 
 app.Run();
