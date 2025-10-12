2aac9f5e0638e10ff0d6f6110fcd24d25c4c9e9d
el2toro <vasea.badragan@gmail.com>
2025-10-12 17:56:07 +0200

    Implement cache in getTasks

--- Services/TaskService/TaskService.Application/Tasks/Queries/GetTasks/GetTasksHandler.cs
+++ Services/TaskService/TaskService.Application/Tasks/Queries/GetTasks/GetTasksHandler.cs
@@ -1,15 +1,28 @@
-﻿namespace TaskService.Application.Tasks.Queries.GetTasks;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace TaskService.Application.Tasks.Queries.GetTasks;
 
 public record GetTasksQuery(Guid ProjectId) : IRequest<GetTasksResult>;
 public record GetTasksResult(IEnumerable<TaskItemDto> Tasks);
 
-public class GetTasksHandler(ITaskServiceRepository taskServiceRepository)
+public class GetTasksHandler(ITaskServiceRepository taskServiceRepository, IDistributedCache distributedCache)
     : IRequestHandler<GetTasksQuery, GetTasksResult>
 {
     public async Task<GetTasksResult> Handle(GetTasksQuery query, CancellationToken cancellationToken)
     {
+        var cachedTasksString = await distributedCache.GetStringAsync("tasks");
+        if (!string.IsNullOrEmpty(cachedTasksString))
+        {
+            var tasks = JsonSerializer.Deserialize<IEnumerable<TaskItemDto>>(cachedTasksString);
+            return new GetTasksResult(tasks!);
+        }
+
         var taskItems = await taskServiceRepository.GetTasksAsync(query.ProjectId, cancellationToken);
         var result = taskItems.Adapt<IEnumerable<TaskItemDto>>();
+
+        var serializedResult = JsonSerializer.Serialize(result);
+        await distributedCache.SetStringAsync("tasks", serializedResult);
 
         return new GetTasksResult(result);
     }
