64474793b2a6dd9ae1515fb463beaa3ed927d831
el2toro <vasea.badragan@gmail.com>
2025-10-13 13:58:28 +0200

    Update project repository

--- Services/Project/Project.API/Repository/IProjectRepository.cs
+++ Services/Project/Project.API/Repository/IProjectRepository.cs
@@ -2,8 +2,9 @@
 
 public interface IProjectRepository
 {
-    Task<Models.Project> CreateProjectAsync(Models.Project project, CancellationToken cancellationToken);
+    Task<Models.Project> CreateProjectAsync(Guid projectId, Models.Project project, CancellationToken cancellationToken);
     Task<(IEnumerable<Models.Project>, int)> GetProjectsAsync(Guid tenantdId, int pageNumber, int pageSize, CancellationToken cancellationToken);
-    Task<ProjectDetailsDto> GetProjectDetailsAsync(Guid projectId, Guid tenantId, CancellationToken cancellationToken);
+    Task<Models.Project> GetProjectByIdAsync(Guid tenantId, Guid projectId, CancellationToken cancellationToken);
     Task<Models.Project> UpdateProjectAsync(ProjectDto projectDto, CancellationToken cancellationToken);
+    Task DeleteProjectAsync(Guid tenantdId, Guid projectId, CancellationToken cancellationToken);
 }
