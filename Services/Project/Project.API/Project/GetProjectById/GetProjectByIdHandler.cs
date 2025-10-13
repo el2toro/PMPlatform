c17a8f24adf36dae3dec950d15b2e2d1725bd6f3
el2toro <vasea.badragan@gmail.com>
2025-10-13 14:02:05 +0200

    Implement caching in getProjectById

--- Services/Project/Project.API/Project/GetProjectById/GetProjectByIdHandler.cs
+++ Services/Project/Project.API/Project/GetProjectById/GetProjectByIdHandler.cs
@@ -1,17 +1,31 @@
-﻿namespace Project.API.Project.GetProjectById;
+﻿using Core.Services;
+
+namespace Project.API.Project.GetProjectById;
 
 public record GetProjectByIdQuery(Guid ProjectId, Guid TenantId) : IRequest<GetProjectByIdResult>;
-public record GetProjectByIdResult(ProjectDetailsDto Project);
+public record GetProjectByIdResult(ProjectDto Project);
 
-public class GetProjectByIdHandler(IProjectRepository projectRepository, UserServiceClient userServiceClient)
+public class GetProjectByIdHandler(IProjectRepository projectRepository,
+    ICacheService cacheService)
     : IRequestHandler<GetProjectByIdQuery, GetProjectByIdResult>
 {
     public async Task<GetProjectByIdResult> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
     {
-        var project = await projectRepository.GetProjectDetailsAsync(request.ProjectId, request.TenantId, cancellationToken);
+        var cachedProject = await cacheService.GetAsync<ProjectDto>(request.ProjectId.ToString(), cancellationToken);
 
-        // var createdByUsers = await userServiceClient.GetUsersByIdAsync(request.TenantId, createdByIds);
+        if (cachedProject is not null)
+        {
+            return new GetProjectByIdResult(cachedProject);
+        }
 
-        return new GetProjectByIdResult(project);
+        var project = await projectRepository.GetProjectByIdAsync(request.TenantId, request.ProjectId, cancellationToken);
+        var projectDto = project.Adapt<ProjectDto>();
+
+        await cacheService.SetAsync<ProjectDto>(request.ProjectId.ToString(),
+            projectDto,
+            cancellationToken,
+            TimeSpan.FromHours(1));
+
+        return new GetProjectByIdResult(projectDto);
     }
 }
