672a8829bc48e62c76950803881db8003ca1273f
el2toro <vasea.badragan@gmail.com>
2025-10-13 20:09:16 +0200

    Create TaskServiceClient

---
+++ Services/Project/Project.API/Services/TaskServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Project.API.Services;
+
+public class TaskServiceClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    public TaskServiceClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+    }
+
+    public async Task<ProjectProgressDto> GetProgresData(Guid tenantId, Guid projectId)
+    {
+        var data = await _httpClient.GetStringAsync($"tenants/{tenantId}/projects/{projectId}/tasks/progress");
+
+        var deserializedData = JsonSerializer.Deserialize<ProjectProgressDto>(data, _jsonSerializerOptions);
+
+        return deserializedData!;
+    }
+}
