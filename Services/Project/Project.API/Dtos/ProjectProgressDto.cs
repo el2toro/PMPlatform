c05623a11f327da3723699944ecabb77d2fb6ef6
el2toro <vasea.badragan@gmail.com>
2025-10-13 20:08:11 +0200

    Add ProjectProgressDto

---
+++ Services/Project/Project.API/Dtos/ProjectProgressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.API.Dtos;
+
+public record ProjectProgressDto
+{
+    public int TotalTasks { get; set; }
+    public int CompletedTasks { get; set; }
+    public int TotalSubtasks { get; set; }
+    public int CompletedSubtasks { get; set; }
+}
