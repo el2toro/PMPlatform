34d3bfc93e549b367940d2e4265926ec5b35f718
el2toro <vasea.badragan@gmail.com>
2025-10-17 18:39:51 +0200

    Update getUsers enpoint and handler

--- Services/Authentication/Auth.API/Auth/Users/GetUsers/GetUsersEndpoint.cs
+++ Services/Authentication/Auth.API/Auth/Users/GetUsers/GetUsersEndpoint.cs
@@ -4,9 +4,17 @@ public class GetUsersEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("users", async (ISender sender) =>
+        app.MapGet("tenants/{tenantId}/users", async (Guid tenantId, ISender sender) =>
         {
-            var result = await sender.Send(new GetUsersQuery());
+            var result = await sender.Send(new GetUsersQuery(tenantId));
+            return Results.Ok(result.Users);
+        })
+        .WithDisplayName("GetUsesByTenantId");
+
+        app.MapGet("tenants/{tenantId}/Projects/{projectId}/users",
+            async (Guid tenantId, Guid projectId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetUsersQuery(tenantId));
             return Results.Ok(result.Users);
         });
     }
