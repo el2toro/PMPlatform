2a39c6630d21b62af6e5cdea54c9064111834be1
el2toro <vasea.badragan@gmail.com>
2025-10-17 18:50:23 +0200

    Add TaskNotfound exception

---
+++ Services/TaskService/TaskService.Domain/Exceptions/TaskNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using Core.Exceptions;
+
+namespace TaskService.Domain.Exceptions;
+
+public class TaskNotFoundException : NotFoundException
+{
+    public TaskNotFoundException(string id) : base("Task", id)
+    {
+    }
+}
