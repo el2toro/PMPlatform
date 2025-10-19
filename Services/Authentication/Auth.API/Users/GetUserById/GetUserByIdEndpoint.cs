80177af1d06878c9c4ff03901851f8aa00b76c8a
el2toro <vasea.badragan@gmail.com>
2025-10-19 16:48:21 +0200

    Implement GetUserById

---
+++ Services/Authentication/Auth.API/Users/GetUserById/GetUserByIdEndpoint.cs
@@ -0,0 +1,15 @@
+﻿namespace Auth.API.Users.GetUsersById;
+
+public record GetUsersByIdRequest(Guid TenantId, IEnumerable<Guid> UserIds);
+
+public class GetUserByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("tenants/{tenantId}/users/{userId}", async (Guid tenantId, Guid userId, ISender sender) =>
+        {
+            var users = await sender.Send(new GetUserByIdQuery(tenantId, userId));
+            return Results.Ok(users.User);
+        });
+    }
+}
---
+++ Services/Authentication/Auth.API/Users/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using Auth.API.Interfaces;
+using Core.CQRS;
+
+namespace Auth.API.Users.GetUsersById;
+
+public record GetUserByIdQuery(Guid TenantId, Guid UserId) : IQuery<GetUserByIdResult>;
+public record GetUserByIdResult(UserDto User);
+
+public class GetUserByIdHandler(IUserRepository userRepository)
+    : IQueryHandler<GetUserByIdQuery, GetUserByIdResult>
+{
+    public async Task<GetUserByIdResult> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        var users = await userRepository.GetUserById(request.TenantId, request.UserId, cancellationToken);
+        var usersDtos = MapToDto(users);
+        return new GetUserByIdResult(usersDtos);
+    }
+
+    //TODO: move to mapper, it is a duplicated method some is in GetUsersByTenantIdHandler
+    private UserDto MapToDto(User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            FullName = $"{user.FirstName} {user.LastName}"
+        };
+    }
+}
