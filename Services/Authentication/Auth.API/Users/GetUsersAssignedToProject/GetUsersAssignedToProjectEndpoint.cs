950c357bd40cdc29d58a9bac98f9c3106998b4f5
el2toro <vasea.badragan@gmail.com>
2025-10-19 16:48:50 +0200

    Implement GetUsersAssignedToProject

---
+++ Services/Authentication/Auth.API/Users/GetUsersAssignedToProject/GetUsersAssignedToProjectEndpoint.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.API.Users.GetUsersAssignedToProject;
+
+public class GetUsersAssignedToProjectEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("tenants/{tenantId}/projects/{projectId}/users",
+            async (Guid tenantId, Guid projectId, [FromQuery] Guid[] userIds, ISender sender) =>
+        {
+            var result = await sender.Send(new GetUsersAssignedToProjectQuery(tenantId, userIds));
+            return Results.Ok(result.Users);
+        });
+    }
+}
---
+++ Services/Authentication/Auth.API/Users/GetUsersAssignedToProject/GetUsersAssignedToProjectHandler.cs
@@ -0,0 +1,30 @@
+﻿using Auth.API.Interfaces;
+using Core.CQRS;
+
+namespace Auth.API.Users.GetUsersAssignedToProject;
+
+public record GetUsersAssignedToProjectQuery(Guid TenantId, IEnumerable<Guid> UserIds) : IQuery<GetUsersAssignedToProjectResult>;
+public record GetUsersAssignedToProjectResult(IEnumerable<UserDto> Users);
+public class GetUsersAssignedToProjectHandler(IUserRepository userRepository)
+    : IQueryHandler<GetUsersAssignedToProjectQuery, GetUsersAssignedToProjectResult>
+{
+    public async Task<GetUsersAssignedToProjectResult> Handle(GetUsersAssignedToProjectQuery query, CancellationToken cancellationToken)
+    {
+        var users = await userRepository.GetUsersById(query.TenantId, query.UserIds, cancellationToken);
+        var result = MapToDto(users);
+        return new GetUsersAssignedToProjectResult(result);
+    }
+
+    //TODO: move to mapper, it is a duplicated method some is in GetUsersByIdHandler
+    private IEnumerable<UserDto> MapToDto(IEnumerable<User> users)
+    {
+        return users.Select(user => new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            FullName = $"{user.FirstName} {user.LastName}"
+        });
+    }
+}
