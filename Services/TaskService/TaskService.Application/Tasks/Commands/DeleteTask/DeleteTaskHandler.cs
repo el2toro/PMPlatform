47689b968b186a39add0ebcccd863cc258576ca7
el2toro <vasea.badragan@gmail.com>
2025-10-20 20:16:44 +0200

    Refactor delete task handler

--- Services/TaskService/TaskService.Application/Tasks/Commands/DeleteTask/DeleteTaskHandler.cs
+++ Services/TaskService/TaskService.Application/Tasks/Commands/DeleteTask/DeleteTaskHandler.cs
@@ -1,14 +1,23 @@
 ﻿namespace TaskService.Application.Tasks.Commands.DeleteTask;
 
-public record DeleteTaskCommand(Guid projectId, Guid TaskId) : IRequest<DeleteTaskResult>;
+public record DeleteTaskCommand(Guid projectId, Guid TaskId) : ICommand<DeleteTaskResult>;
 public record DeleteTaskResult(bool IsSuccess);
 
-public class DeleteTaskHandler(ITaskServiceRepository taskServiceRepository)
-    : IRequestHandler<DeleteTaskCommand, DeleteTaskResult>
+public class DeleteTaskHandler(ITaskServiceRepository taskServiceRepository,
+    IPublishEndpoint publishEndpoint)
+    : ICommandHandler<DeleteTaskCommand, DeleteTaskResult>
 {
     public async Task<DeleteTaskResult> Handle(DeleteTaskCommand command, CancellationToken cancellationToken)
     {
-        await taskServiceRepository.DeleteTaskAsync(command.TaskId, cancellationToken);
+        var existingTask = await taskServiceRepository
+            .GetTaskByIdAsync(command.projectId, command.TaskId, cancellationToken)
+            ?? throw new TaskNotFoundException(command.TaskId.ToString());
+
+        await taskServiceRepository.DeleteTaskAsync(existingTask, cancellationToken);
+
+        //Notify about deleted task
+        await publishEndpoint.Publish(new TaskDeletedEvent(existingTask.Id), cancellationToken);
+
         return new DeleteTaskResult(true);
     }
 }
