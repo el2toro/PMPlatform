b150f57e4750cf5696a4910247aaf3c7316fd949
el2toro <vasea.badragan@gmail.com>
2025-10-20 20:17:23 +0200

    Add task deleted event handler

---
+++ Services/TaskService/TaskService.Application/Tasks/EventHandlers/TaskDeletedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskService.Application.Tasks.EventHandlers;
+
+public class TaskDeletedEventHandler(ILogger<TaskDeletedEventHandler> logger, ITaskServiceHub taskServiceHub)
+    : IConsumer<TaskDeletedEvent>
+{
+    public async Task Consume(ConsumeContext<TaskDeletedEvent> context)
+    {
+        logger.LogInformation("Task Event Handler: {TaskCreatedEvent}", context.Message.GetType().Name);
+
+        Guid taskId = context.Message.Id;
+        await taskServiceHub.SendTaskDeleted(taskId);
+    }
+}
