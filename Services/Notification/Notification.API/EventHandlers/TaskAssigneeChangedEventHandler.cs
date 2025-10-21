9ab49d444a69ecdf4cef8f2cf80d95fac73ef722
el2toro <vasea.badragan@gmail.com>
2025-10-21 18:38:16 +0200

    Implement TaskAssigneeChangedEventHandler

---
+++ Services/Notification/Notification.API/EventHandlers/TaskAssigneeChangedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using Core.Messaging.Events.Task;
+using MassTransit;
+using Notification.API.Services;
+
+namespace Notification.API.EventHandlers;
+
+public class TaskAssigneeChangedEventHandler(EmailService emailService)
+    : IConsumer<TaskAssigneeChangedEvent>
+{
+    public async Task Consume(ConsumeContext<TaskAssigneeChangedEvent> context)
+    {
+
+        string to = "application.service001@gmail.com";
+        string subject = "New task assigned";
+        string body = $"You have a new task to do: Task id: {context.Message.Id}, Task name: {context.Message.Title}";
+        await emailService.SendEmailAsync(to, subject, body);
+
+    }
+}
