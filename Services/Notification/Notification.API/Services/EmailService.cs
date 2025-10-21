a38c711ccdb03a5ecbda245fb716ce15ada76612
el2toro <vasea.badragan@gmail.com>
2025-10-21 18:40:49 +0200

    Implement email service

---
+++ Services/Notification/Notification.API/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Notification.API.Services;
+
+public class EmailService(IConfiguration configuration)
+{
+    public async Task SendEmailAsync(string sendTo, string subject, string body)
+    {
+        var client = new SendGridClient(configuration["SendGrid:ApiKey"]);
+        var from = new EmailAddress("application.service001@gmail.com", "Project Manager");
+        var to = new EmailAddress(sendTo);
+        var email = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+
+        var response = await client.SendEmailAsync(email);
+    }
+}
