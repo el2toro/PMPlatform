cc7f70ead10b0b6adf047fc0ca0c0ce3b86daeb2
el2toro <vasea.badragan@gmail.com>
2025-10-25 09:46:54 +0200

    Implement getuser assigned task handler

---
+++ Services/TaskService/TaskService.Application/Tasks/Queries/GetUserAssignedTasks/GetUserAssignedTasksHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskService.Application.Tasks.Queries.GetUserAssignedTasks;
+
+public record GetUserAssignedTasksQuery(Guid TenantId, Guid UserId) : IQuery<GetUserAssignedTasksResult>;
+public record GetUserAssignedTasksResult(IEnumerable<TaskItemDto> Tasks);
+internal class GetUserAssignedTasksHandler(ITaskServiceRepository taskServiceRepository)
+    : IQueryHandler<GetUserAssignedTasksQuery, GetUserAssignedTasksResult>
+{
+    public async Task<GetUserAssignedTasksResult> Handle(GetUserAssignedTasksQuery query, CancellationToken cancellationToken)
+    {
+        var tasks = await taskServiceRepository.GetUserAssigendTasksAsync(query.TenantId, query.UserId, cancellationToken);
+        var result = tasks.Adapt<IEnumerable<TaskItemDto>>();
+
+        return new GetUserAssignedTasksResult(result);
+    }
+}
