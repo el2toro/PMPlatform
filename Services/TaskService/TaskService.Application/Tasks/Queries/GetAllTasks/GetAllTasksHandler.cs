bdf740d4564a1c7a2c460c845239d6721eb55f66
el2toro <vasea.badragan@gmail.com>
2025-10-25 09:46:26 +0200

    Implement get all tasks handler

---
+++ Services/TaskService/TaskService.Application/Tasks/Queries/GetAllTasks/GetAllTasksHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskService.Application.Tasks.Queries.GetAllTasks;
+
+public record GetAllTasksQuery(Guid TenantId) : IQuery<GetAllTasksResult>;
+public record GetAllTasksResult(IEnumerable<TaskItemDto> Tasks);
+
+internal class GetAllTasksHandler(ITaskServiceRepository taskServiceRepository)
+    : IQueryHandler<GetAllTasksQuery, GetAllTasksResult>
+{
+    public async Task<GetAllTasksResult> Handle(GetAllTasksQuery query, CancellationToken cancellationToken)
+    {
+        var tasks = await taskServiceRepository.GetAllTasksAsync(query.TenantId, cancellationToken);
+        var result = tasks.Adapt<IEnumerable<TaskItemDto>>();
+
+        return new GetAllTasksResult(result);
+    }
+}
