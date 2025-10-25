c96e7b293e7dad2c879823bcfcce75e561c90eeb
el2toro <vasea.badragan@gmail.com>
2025-10-25 09:48:33 +0200

    Refactor logout and register handlers

--- Services/Authentication/Auth.API/Auth/Logout/LogoutHandler.cs
+++ Services/Authentication/Auth.API/Auth/Logout/LogoutHandler.cs
@@ -1,16 +1,21 @@
-﻿using Auth.API.Interfaces;
+﻿namespace Auth.API.Auth.Logout;
 
-namespace Auth.API.Auth.Logout;
-
-public record LogoutCommand(string RefreshToken, Guid TenantId) : IRequest<LogoutResult>;
+public record LogoutCommand(string RefreshToken, Guid TenantId) : ICommand<LogoutResult>;
 public record LogoutResult(bool IsSuccess);
 
-public class LogoutHandler(IAuthRepository repository)
-    : IRequestHandler<LogoutCommand, LogoutResult>
+public class LogoutHandler(IAuthRepository authRepository)
+    : ICommandHandler<LogoutCommand, LogoutResult>
 {
     public async Task<LogoutResult> Handle(LogoutCommand command, CancellationToken cancellationToken)
     {
-        await repository.Logout(command.RefreshToken, command.TenantId, cancellationToken);
+        var existingRefreshToken = await authRepository
+            .GetRefreshTokenAsync(command.TenantId, command.RefreshToken, cancellationToken)
+            ?? throw new RefreshTokenNotFoundException("Refresh token was not found");
+
+        existingRefreshToken.RevokedAt = DateTime.UtcNow;
+
+        await authRepository.Logout(existingRefreshToken, cancellationToken);
+
         return new LogoutResult(true);
     }
 }
--- Services/Authentication/Auth.API/Auth/Register/RegisterHandler.cs
+++ Services/Authentication/Auth.API/Auth/Register/RegisterHandler.cs
@@ -1,34 +1,39 @@
-﻿using Auth.API.Interfaces;
-
-namespace Auth.API.Auth.Register;
+﻿namespace Auth.API.Auth.Register;
 
 public record RegisterCommand(
     string FirstName,
     string LastName,
     string Email,
     string Password,
-    string TenantName) : IRequest<RegisterResult>;
+    string TenantName) : ICommand<RegisterResult>;
 public record RegisterResult(RegisterResponseDto RegisterDto);
 
-public class RegisterHandler(IAuthRepository repository, TenantServiceClient tenantServiceClient)
-    : IRequestHandler<RegisterCommand, RegisterResult>
+public class RegisterHandler(IUserRepository userRepository,
+    TenantServiceClient tenantServiceClient)
+    : ICommandHandler<RegisterCommand, RegisterResult>
 {
     public async Task<RegisterResult> Handle(RegisterCommand command, CancellationToken cancellationToken)
     {
-        RegisterRequestDto registerDto = command.Adapt<RegisterRequestDto>();
-        User createdUser = await repository.RegisterUser(registerDto, cancellationToken);
+        User user = await userRepository.GetUserByEmail(command.Email, cancellationToken);
 
-        Tenant? tenant = await tenantServiceClient.GetTenantByName(registerDto.TenantName);
+        //Cannot create a user if a user with the same email already exists
+        if (user is not null)
+            throw new InvalidOperationException($"User with this email: {nameof(command.Email)} already exists.");
 
-        Tenant tenantRequest = MapDtoToTenant(registerDto.TenantName, createdUser.Id);
+        user = MapCommandToUser(command);
+        User registeredUser = await userRepository.RegisterUser(user, cancellationToken);
 
-        // Create tenant if it does not exist
+        //Get potential tenant where user will be assigned
+        Tenant? tenant = await tenantServiceClient.GetTenantByName(command.TenantName);
+        Tenant tenantRequest = MapDtoToTenant(command.TenantName, registeredUser!.Id);
+
+        // Create tenant if it does not exists
         tenant ??= await tenantServiceClient.CreateTenant(tenantRequest);
 
-        // Add user to tenant as admin, since they created the tenant, to be reviewed
-        await repository.AddUserToTenant(tenant.TenantId, createdUser.Id, TenantRole.Admin, cancellationToken);
+        // Add user to tenant as owner, since they created the tenant
+        await userRepository.AddUserToTenant(tenant.TenantId, registeredUser.Id, TenantRole.Owner, cancellationToken);
 
-        RegisterResponseDto result = MapUserToRegisterDto(createdUser, tenant.Name);
+        RegisterResponseDto result = MapUserToRegisterDto(registeredUser, tenant.Name);
 
         return new RegisterResult(result);
     }
@@ -46,10 +51,23 @@ public class RegisterHandler(IAuthRepository repository, TenantServiceClient ten
         return new RegisterResponseDto
         {
             UserId = user.Id,
-            FirstName = user.FirstName,
-            LastName = user.LastName,
-            Email = user.Email,
+            FirstName = user?.FirstName!,
+            LastName = user?.LastName!,
+            Email = user?.Email!,
             TenantName = tenantName
+        };
+    }
+
+    private User MapCommandToUser(RegisterCommand command)
+    {
+        return new User
+        {
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+            Email = command.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(command.Password),
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
         };
     }
 }
