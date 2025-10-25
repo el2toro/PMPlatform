907231698b938ec2438e2e19e80f901efd5326d8
el2toro <vasea.badragan@gmail.com>
2025-10-25 09:53:33 +0200

    Add user repository implementations

--- Services/Authentication/Auth.API/Interfaces/IUserRepository.cs
+++ Services/Authentication/Auth.API/Interfaces/IUserRepository.cs
@@ -8,4 +8,9 @@ public interface IUserRepository
     Task<User> GetUserByEmail(string email, CancellationToken cancellationToken);
     Task<IEnumerable<User>> GetUsersById(Guid tenantId, IEnumerable<Guid> userIds, CancellationToken cancellationToken);
     Task<User> GetUserById(Guid tenantId, Guid userId, CancellationToken cancellationToken);
+    Task AddUserToTenant(Guid tenantId, Guid userId, TenantRole role, CancellationToken cancellationToken);
+    Task<IEnumerable<User>> GetUsersByTenantId(Guid tenantId, CancellationToken cancellationToken);
+    Task DeleteUserFromTenant(Guid tenantId, Guid userId, CancellationToken cancellationToken);
+    Task DeleteUser(Guid tenantId, Guid userId, CancellationToken cancellationToken);
+    Task<User> RegisterUser(User user, CancellationToken cancellationToken);
 }
--- Services/Authentication/Auth.API/Repositories/UserRepository.cs
+++ Services/Authentication/Auth.API/Repositories/UserRepository.cs
@@ -1,8 +1,4 @@
-﻿
-using Auth.API.Interfaces;
-using Microsoft.EntityFrameworkCore;
-
-namespace Auth.API.Repository;
+﻿namespace Auth.API.Repositories;
 
 public class UserRepository(AuthDbContext authDbContext) : IUserRepository
 {
@@ -44,7 +40,7 @@ public class UserRepository(AuthDbContext authDbContext) : IUserRepository
             .Include(u => u.UserTenants)
             .AsNoTracking()
             .Where(u => u.Email == email)
-            .FirstOrDefaultAsync(cancellationToken);
+            .FirstOrDefaultAsync(cancellationToken) ?? default!;
     }
 
     public async Task<IEnumerable<User>> GetUsersById(Guid tenantId, IEnumerable<Guid> userIds, CancellationToken cancellationToken)
@@ -60,6 +56,48 @@ public class UserRepository(AuthDbContext authDbContext) : IUserRepository
         return await authDbContext.Users
             .AsNoTracking()
             .Where(u => u.UserTenants.Any(ut => ut.TenantId == tenantId) && u.Id == userId)
-            .FirstOrDefaultAsync(cancellationToken);
+            .FirstOrDefaultAsync(cancellationToken) ?? default!;
+    }
+
+    public async Task AddUserToTenant(Guid tenantId, Guid userId, TenantRole role, CancellationToken cancellationToken)
+    {
+        authDbContext.UserTenants.Add(new UserTenant
+        {
+            TenantId = tenantId,
+            UserId = userId,
+            Role = role
+        });
+
+        await authDbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<User>> GetUsersByTenantId(Guid tenantId, CancellationToken cancellationToken)
+    {
+        return await authDbContext.Users
+            .AsNoTracking()
+            .Where(u => u.UserTenants.Any(ut => ut.TenantId == tenantId))
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task DeleteUserFromTenant(Guid tenantId, Guid userId, CancellationToken cancellationToken)
+    {
+        await authDbContext.UserTenants
+            .Where(ut => ut.TenantId == tenantId && ut.UserId == userId)
+            .ExecuteDeleteAsync(cancellationToken);
+    }
+
+    public async Task DeleteUser(Guid tenantId, Guid userId, CancellationToken cancellationToken)
+    {
+        await authDbContext.Users
+            .Where(u => u.Id == userId && u.UserTenants.Any(ut => ut.TenantId == tenantId))
+            .ExecuteDeleteAsync(cancellationToken);
+    }
+
+    public async Task<User> RegisterUser(User user, CancellationToken cancellationToken)
+    {
+        var createdUser = authDbContext.Users.Add(user).Entity;
+        await authDbContext.SaveChangesAsync(cancellationToken);
+
+        return createdUser;
     }
 }
