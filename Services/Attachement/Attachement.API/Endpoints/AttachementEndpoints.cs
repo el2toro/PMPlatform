e1a33e1dc930b8304b1d09027451ae7569a460ae
el2toro <vasea.badragan@gmail.com>
2025-10-27 15:22:21 +0100

    Move attachement project to services

---
+++ Services/Attachement/Attachement.API/Endpoints/AttachementEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+using MediatR;
+
+
+namespace Attachement.API.Endpoints;
+
+public class AttachementEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/tenants/{tenantId}/projects/{projectId}/attachements/{attachementId}",
+            (ISender sender) => "Hello from Attachement API!");
+
+        app.MapPost("/tenants/{tenantId}/projects/{projectId}/attachements/",
+           (ISender sender) => "Add Attachement API!");
+
+        app.MapDelete("/tenants/{tenantId}/projects/{projectId}/attachements/{attachementId}",
+           (ISender sender) => "Hello from Attachement API!");
+    }
+}
